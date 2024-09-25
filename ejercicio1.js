e87ae95f10c07db6c5ed98bd72d8b1fb8a6dee83
Yhiazer <111998756+Yhiazer@users.noreply.github.com>
2024-09-25 15:36:57 -0300

    Create ejercicio1.js

---
+++ ejercicio1.js
@@ -0,0 +1,14 @@
+//https://www.codewars.com/kata/55f4a44eb72a0fa91600001e
+
+function createMessage(palabra) {
+  let palabras = [palabra];
+  function concatenar(palabra){
+    if (palabra === undefined) {
+    return palabras.join(" ");
+  } else {
+    palabras.push(palabra);
+    return concatenar;
+  }
+}
+  return concatenar;
+  }
